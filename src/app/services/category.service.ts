f85ee2b6e9a20f878cb4a67d2e2904a38a8c2fbb
Shohidul Islam <hridoy.jurain@yahoo.com>
2024-12-05 14:14:15 +0600

    Update src/app/services/category.service.ts [BoltSync]

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { DbService } from './db.service';
+import { defaultCategories } from '../data/category-icons';
+
+@Injectable({ providedIn: 'root' })
+export class CategoryService {
+  constructor(private dbService: DbService) {}
+
+  async initializeDefaultCategories() {
+    const existingCategories = await this.dbService.getCategories();
+
+    for (const category of defaultCategories) {
+      const exists = existingCategories.some(
+        c =>
+          c.icon === category.icon &&
+          c.type === category.type &&
+          c.isCustom === false // Only check default categories
+      );
+
+      if (!exists) {
+        await this.dbService.addCategory({
+          name: category.name,
+          icon: category.icon,
+          type: category.type,
+          isCustom: false
+        });
+      }
+    }
+  }
+
+  async getAllCategories() {
+    return await this.dbService.getCategories();
+  }
+}
