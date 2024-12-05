d43b7abfee33e7fa00ecf8cdd5b0ff96c521e10b
Shohidul Islam <hridoy.jurain@yahoo.com>
2024-12-05 14:14:09 +0600

    Update src/app/app.config.ts [BoltSync]

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { CategoryService } from './services/category.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes)
+    provideRouter(routes),
+    CategoryService, // Ensure the service is provided globally
   ]
 };
