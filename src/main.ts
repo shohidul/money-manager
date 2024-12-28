a40ab132f3352f32bb2de42663c7f0d0d812f33c
Shohidul Islam <hridoy.jurain@yahoo.com>
2024-12-29 02:56:10 +0600

    pwa

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,18 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { TranslatePipe } from './app/components/shared/translate.pipe';
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
     provideRouter(routes),
     TranslatePipe // Providing TranslatePipe
-  ]
+    ,
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 }).catch(err => console.error(err));
